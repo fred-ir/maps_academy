0a16cf8abbd5b188f58842b1eeb995456f1662ce
Frederic Morel <frederic.morel@iphc.cnrs.fr>
2025-05-28 06:50:41 +0200

    tools setup

---
+++ lab/tools_setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env tcsh
+
+################################################
+#     PDK setup file for tcsh environement     #
+################################################
+
+# Check if file is sourced
+set sourced = ($_)
+if ("$sourced" == "") then
+   echo "Script should be sourced."
+   exit 1
+endif
+
+# Find out in which institute the script is being called
+if ($HOST =~ sbgue*.in2p3.fr) then
+  echo Found IPHC cluster: $HOST
+  set CLUSTER="iphc"
+  set TOOLS_EUROP_VERSION="2024-25"
+endif
+
+source ${CLUSTER}.modules.europractice.${TOOLS_EUROP_VERSION}.sh
